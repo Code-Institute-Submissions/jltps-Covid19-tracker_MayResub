acb7dfa315f9e773d34ae43cd91e25383832385f
JLTPS <jlts2010@gmail.com>
2020-12-02 13:35:44 +0000

    Display Covid 19 API data in body indicators

--- assets/scripts/covid19api.js
+++ assets/scripts/covid19api.js
@@ -1,9 +1,28 @@
-var settings = {
-    "url": "https://api.covid19api.com/summary",
-    "method": "GET",
-    "timeout": 0,
-};
+function getData(cb) {
+    var settings = {
+        "url": "https://api.covid19api.com/summary",
+        "method": "GET",
+        "timeout": 0,
+    };
 
-$.ajax(settings).done(function(response) {
-    console.log(response);
+
+    $.ajax(settings).done(function(response) {
+        cb(response);
+    });
+}
+
+getData(function(data) {
+    console.log(data);
+    console.dir(data);
 });
+
+function writeToDocument() {
+    getData(function(data) {
+        document.getElementById("cases-number").innerHTML = data.Global.TotalConfirmed;
+        document.getElementById("active-number").innerHTML = data.Global.NewConfirmed;
+        document.getElementById("deaths-number").innerHTML = data.Global.TotalDeaths;
+        document.getElementById("recovered-number").innerHTML = data.Global.TotalRecovered;
+    });
+}
+
+writeToDocument();
