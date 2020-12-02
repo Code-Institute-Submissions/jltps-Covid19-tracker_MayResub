a7de3134b1fce60ca1e1c6c669a8247cb8c4e742
JLTPS <jlts2010@gmail.com>
2020-12-02 14:26:24 +0000

    Adjust spacing, colours and text format in body indicators

--- assets/scripts/covid19api.js
+++ assets/scripts/covid19api.js
@@ -18,10 +18,10 @@ getData(function(data) {
 
 function writeToDocument() {
     getData(function(data) {
-        document.getElementById("cases-number").innerHTML = data.Global.TotalConfirmed;
-        document.getElementById("active-number").innerHTML = data.Global.NewConfirmed;
-        document.getElementById("deaths-number").innerHTML = data.Global.TotalDeaths;
-        document.getElementById("recovered-number").innerHTML = data.Global.TotalRecovered;
+        document.getElementById("cases-number").innerHTML = data.Global.TotalConfirmed.toLocaleString();
+        document.getElementById("active-number").innerHTML = data.Global.NewConfirmed.toLocaleString();
+        document.getElementById("deaths-number").innerHTML = data.Global.TotalDeaths.toLocaleString();
+        document.getElementById("recovered-number").innerHTML = data.Global.TotalRecovered.toLocaleString();
     });
 }
 
