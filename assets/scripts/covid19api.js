6be549d222706fde5cd312e2e18d2b4ba126e34a
José Luís Sousa <70488865+jltps@users.noreply.github.com>
2021-05-05 17:40:33 +0100

    Add fetchCountryName() to covid19api.js

--- assets/scripts/covid19api.js
+++ assets/scripts/covid19api.js
@@ -1,4 +1,5 @@
-const summaryURL = "https://api.covid19api.com/summary"
+const summaryURL = "https://api.covid19api.com/summary";
+const countryListURL = "https://api.covid19api.com/countries";
 
 const getData = async () => {
     const response = await fetch(summaryURL);
@@ -23,8 +24,12 @@ const writeSummaryToDocument = async () => {
 writeSummaryToDocument();
 
 const fetchCountryName = async (event) => {
-    let countryName = $("#coutryNameInput").val();
-    if (!countryName) {
+    const response = await fetch(countryListURL);
+    const data = await response.json();
+    let countryNameInput = $("#coutryNameInput").val();
+    console.log(data, 'countryName');
+    
+    if (!countryNameInput) {
         $("#searchCountryData").html(`<h5>Please enter a Country name</h5>`);
         return;
     }
@@ -32,25 +37,17 @@ const fetchCountryName = async (event) => {
     $("#searchCountryData").html(
         `<div id="loader">
             <img src="assets/css/loader.gif" alt="loading..." />
-        </div>`)
+        </div>`);
+
+    let foundCountries = [];
+    for(let i = 0; i < data.length; i++) {
+        if(data[i].Country.toLowerCase().includes(countryNameInput.toLowerCase()))
+            foundCountries.push(data[i]);
+    }
+    
+    for(let i=0; i < foundCountries.length; i++) {
+        $("#searchCountryData").append(`<button class="btn">${foundCountries[i].Country}</button>`)
+    }
+    console.log(foundCountries, "found country")
+    
 }
-
-// function getCountriesList() {
-//     let url = "https://api.covid19api.com/countries";
-//     let cl = $.getJSON(url);
-//     let clparsed = JSON.parse(cl);
-//     console.log("CL is: " + clparsed);
-// }
-
-// getCountriesList();
-
-// function writeToDocument() {
-//     getSummary(function(data) {
-//         document.getElementById("cases-number").innerHTML = data.Global.TotalConfirmed.toLocaleString();
-//         document.getElementById("active-number").innerHTML = data.Global.NewConfirmed.toLocaleString();
-//         document.getElementById("deaths-number").innerHTML = data.Global.TotalDeaths.toLocaleString();
-//         document.getElementById("recovered-number").innerHTML = data.Global.TotalRecovered.toLocaleString();
-//     });
-// }
-
-// writeToDocument();
